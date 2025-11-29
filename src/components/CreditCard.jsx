0d704639f5e610f5d375814fb099e5a69211a48c
jorgejciud <jorge.julio@iudigital.edu.co>
2025-11-28 19:48:51 -0500

    Fuente de datos y cards

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import { createDefaultImportMeta } from 'vite/module-runner'
 
-const CreditCard = () => {
+
+export const CreditCard = ({ credit }) => {
+
+  const {
+    name, 
+    description,
+    minAmount,
+    maxAmount,
+    interestRate,
+    maxTerm,
+    requirements,
+    icon,
+  } = credit;
+
   return (
-    <div>
+    <div className='credit-card'>
+
+      <div className='card-header'>
+        <span className='icon'>{icon}</span>
+        <h4>{name}</h4>
+      </div>
       
+      <p className='description'>{description}</p>
+
+      <div className='details'>
+        <div className='detail-item'>
+          <span className='label'>Tasa de interés:</span>
+          <span className='value'>{interestRate} % mensual</span>
+        </div>
+
+        <div className='detail-item'>
+          <span className='label'>Monto:</span>
+          <span className='value'> 
+            {formatCurrency(minAmount) - formatCurrency(maxAmount)}
+          </span>
+        </div>
+
+        <div className='detail-item'>
+          <span className='label'>Plazo:</span>
+          <span className='value'>Hasta {maxTerm} meses</span>
+        </div>
+      </div>
+
+      <button className='btn-primary'>Solicitar Ahora</button>
+
     </div>
-  )
+    
+
+  );
 }
 
-export default CreditCard
---
+++ src/data/creditsData.js
@@ -0,0 +1,68 @@
+export const creditsData = [
+    {
+        id: 1,
+        name: "Crédito de libre inversión",
+        description: "Dinero más rápido",
+        minAmount: 1000000,
+        maxAmount: 30000000,
+        interestRate: 1.8,
+        maxTerm: 60,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    },
+    {
+        id: 2,
+        name: "Crédito de Vehículo",
+        description: "Dinero más rápido",
+        minAmount: 10000000,
+        maxAmount: 80000000,
+        interestRate: 1.5,
+        maxTerm: 84,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    },
+    {
+        id: 3,
+        name: "Crédito Vivienda",
+        description: "Dinero más rápido",
+        minAmount: 20000000,
+        maxAmount: 5000000000,
+        interestRate: 0.9,
+        maxTerm: 240,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    },
+    {
+        id: 4,
+        name: "Crédito Educativo",
+        description: "Dinero más rápido",
+        minAmount: 2000000,
+        maxAmount: 40000000,
+        interestRate: 1.2,
+        maxTerm: 72,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    },
+    {
+        id: 5,
+        name: "Crédito Empresarial",
+        description: "Dinero más rápido",
+        minAmount: 500000,
+        maxAmount: 30000000,
+        interestRate: 1.6,
+        maxTerm: 120,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    },
+    {
+        id: 6,
+        name: "Crédito de consumo",
+        description: "Dinero más rápido",
+        minAmount: 500000,
+        maxAmount: 15000000,
+        interestRate: 2.1,
+        maxTerm: 60,
+        requirements: "Mayor de 18 años, ingresos mínimos de $1.200.000",
+        icon: ""
+    }
+]
